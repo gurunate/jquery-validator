588810eba85ee5f1d5f61bc5881dd8408434fcad
unknown <Nate@.(none)>
2012-11-18 20:13:33 -0600

    JSLint clean-up.

--- js/jquery.validator.src.js
+++ js/jquery.validator.src.js
@@ -1,31 +1,34 @@
-/*!
+/** @license
  * validator - Form validation with jQuery
- *
- * Copyright (c) 2011 Media 309. (http://www.media309.com)
+ * Author: Nate Johnson
+ * Version: 0.1.0 (Nov 2012)
+ * Released under the MIT license
  */
 
-(function($) {
-	var errors = "";
-	
-	$.fn.extend({
-		validator: function(options) {
+(function ($) {
+	"use strict";
 
-			if ((typeof options == 'object') || (options == undefined)) {
+	var errors = '';
+
+	$.fn.extend({
+		validator: function (options) {
+
+			if ((typeof options === 'object') || (options === undefined)) {
 				var defaults = {
 					flag: false,
-					required: "",
-					highlight: "",
-					symbol: "*",
+					required: '',
+					highlight: '',
+					symbol: '*',
 					footnote: '<p><span class="requiredFlag">*</span> <em>denotes a required field.</em></p>'
 				}
 				
 				var $o = $.extend(defaults, options);
 				
-				if ($o.required != "") {
+				if ($o.required != '') {
 					insertClass("requiredFlag", $o.required);
 				}
 				
-				if ($o.highlight != "") {
+				if ($o.highlight != '') {
 					insertClass("validator-highlight", $o.highlight);
 				}
 				
@@ -33,32 +36,32 @@
 					if (options.footnote != undefined) {
 						$o.footnote = '<p><span class="requiredFlag">' + $o.symbol + '</span> <em>' + options.footnote + '</em></p>';
 					
-					} else if (options.symbol != "") {
-						$o.footnote = $o.footnote.replace("*", $o.symbol);
+					} else if (options.symbol != '') {
+						$o.footnote = $o.footnote.replace('*', $o.symbol);
 					}
 				}
 	
-				return this.each(function() {
+				return this.each(function () {
 					denote($(this), $o);
 	
 					var validatedOnce = false;
-					$(this).submit(function() {
+					$(this).submit(function () {
 						validatedOnce = true;
 						errors = checkValues($(this), $o);
 						if (errors.length > 0) {
-							if ($o.error == undefined) {
+							if ($o.error === undefined) {
 								showErrors(errors);
-							} else if (typeof $o.error == 'function') {
+							} else if (typeof $o.error === 'function') {
 								$o.error.call(this, errors);
 							}
 							return false;
 						} else if ($o.rules != undefined) {
 							return satisfiesRules($(this), $o);
 						} else {
-							if (($o.success != undefined) && (typeof $o.success == 'function')) {
+							if (($o.success != undefined) && (typeof $o.success === 'function')) {
 								$o.success.call(this);
 								
-								if ($o.submit == undefined) {
+								if ($o.submit === undefined) {
 									$o.submit = false;
 								}
 							}
@@ -66,11 +69,11 @@
 						}
 					});
 	
-					$(this).bind('keyup', function() {
+					$(this).bind('keyup', function () {
 						if (validatedOnce) {
 							errors = checkValues($(this), $o);
 							
-							if (($o.live != undefined) && (typeof $o.live == 'function')) {
+							if (($o.live != undefined) && (typeof $o.live === 'function')) {
 								$o.live.call(this, errors);
 							}
 						}
@@ -78,11 +81,11 @@
 				});
 			} else {
 				switch(options) {
-					case "validate":
+					case 'validate':
 						return validate($(this));
 						break;
 						
-					case "getErrors":
+					case 'getErrors':
 						return getErrors();
 						break;
 				}
@@ -92,9 +95,8 @@
 
 	function denote($form, $o) {
 		var hasRequired = false;
-		$form.find(':input.required').each(function() {
+		$form.find(':input.required').each(function () {
 			if ($(this).attr('id')) {
-//				var $label = $form.find('label[for=' + $(this).attr('id') + ']'); 
 				var $label = $('label[for=' + $(this).attr('id') + ']'); 
 				if ($label.parent().html().indexOf('class="requiredFlag"') < 0) {
 					hasRequired = true;
@@ -112,9 +114,8 @@
 		var retval = new Array();
 
 		$form.find(':input.required').each(
-				function() {
-					if ($(this).val() == "") {
-//						var label = $form.find('label[for=' + $(this).attr('id') + ']').html();
+				function () {
+					if ($(this).val() === '') {
 						var label = $('label[for=' + $(this).attr('id') + ']').html();
 						retval.push({
 							id: $(this).attr('id'),
@@ -127,10 +128,10 @@
 					}
 				});
 
-		var $last = "";
+		var $last = '';
 		$form.find(':input.required').filter('.password').each(
-				function() {
-					if (($last != "") && ($last.val() != $(this).val())) {
+				function () {
+					if (($last != '') && ($last.val() != $(this).val())) {
 						retval.push({
 							id: $(this).attr('id'),
 							message: '<strong>' + $('label[for=' + $last.attr('id') + ']').html() + '</strong> and <strong>' + $('label[for=' + $(this).attr('id') + ']').html() + '</strong> are not equal.'
@@ -150,7 +151,7 @@
 	function validate(form) {
 		errors = checkValues(form);
 		
-		form.bind("keyup", function() {
+		form.bind('keyup', function () {
 			checkValues($(this));
 		});
 		
@@ -166,7 +167,7 @@
 			$('head').append('<style></style>');
 		}
 		
-		var rules = "";
+		var rules = '';
 		for (s in styles) {
 			rules += s + ': ' + styles[s] + ';'
 		}
@@ -175,9 +176,9 @@
 	}
 	
 	function showErrors(errors) {
-		var msg = "Correct the following to proceed:\n\n";
+		var msg = 'Correct the following to proceed:\n\n';
 		for (var i=0; i<errors.length; i++) {
-			msg += " - " + errors[i].label + " field is required.\n";
+			msg += ' - ' + errors[i].label + ' field is required.\n';
 		}
 		alert(msg);
 	}
@@ -196,17 +197,15 @@
 	
 	function isValidRule($form, $r) {
 		switch($r.oper) {
-			case "not equal":
+			case 'not equal':
 				
-				$input1 = $form.find("#" + $r.input[0]);
-				$input2 = $form.find("#" + $r.input[1]);
+				$input1 = $form.find('#' + $r.input[0]);
+				$input2 = $form.find('#' + $r.input[1]);
 				if ($input1.val() != $input2.val()) {
-//					var label1 = $form.find('label[for=' + $r.input[0] + ']').html();
-//					var label2 = $form.find('label[for=' + $r.input[1] + ']').html();
 					var label1 = $('label[for=' + $r.input[0] + ']').html();
 					var label2 = $('label[for=' + $r.input[1] + ']').html();
 					
-					if (($r.error != undefined) && (typeof $r.error == 'function')) {
+					if (($r.error != undefined) && (typeof $r.error === 'function')) {
 						$r.error.call(this, {
 							id: [$r.input[0], $r.input[1]],
 							label: [label1, label2],
@@ -214,7 +213,7 @@
 						});
 					}
 					return false;
-				} else if (($r.success != undefined) && (typeof $r.success == 'function')) {
+				} else if (($r.success != undefined) && (typeof $r.success === 'function')) {
 					$r.success.call(this, errors);
 				}
 				break;
